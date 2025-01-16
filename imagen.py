6fedc960bf7a0d3daf5524387e635c0e6409fd79
Edwin75206 <edwinj3chidoman@gmail.com>
2025-01-15 20:16:49 -0600

    Añadir mis archivos de código

---
+++ imagen.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+# Cargar la imagen dañada
+damaged = cv2.imread("/Users/macbookair/Downloads/deadpool.png")
+cv2.imshow("Damaged Image", damaged)  
+
+# Cargar la máscara de la imagen
+damaged_mask = cv2.imread("/Users/macbookair/Downloads/deadpool.png")
+cv2.imshow("Damaged Mask", damaged_mask)  
+
+# Aplicar umbral
+threshold_value = 254
+output_value = 255
+ret, thresh = cv2.threshold(damaged_mask, threshold_value, output_value, cv2.THRESH_BINARY)
+cv2.imshow("Thresholded Image", thresh) 
+
+# Crear un kernel para la dilatación
+kernel = np.ones((3, 3), np.uint8)
+mask = cv2.dilate(thresh, kernel, iterations=1)
+gray_mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Mask", gray_mask) 
+
+# Restaurar la imagen
+inpaint_radius = 3
+restored = cv2.inpaint(damaged, gray_mask, inpaint_radius, cv2.INPAINT_NS)
+cv2.imshow("Restored Image", restored)  
+
+# Guardar la imagen restaurada
+cv2.imwrite("/Users/macbookair/Downloads/restored_deadpool.png", restored)  # Solo un slash
+
+# Esperar a que se presione una tecla y cerrar las ventanas
+cv2.waitKey(0)
+cv2.destroyAllWindows()
